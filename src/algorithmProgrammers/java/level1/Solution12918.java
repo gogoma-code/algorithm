56adf0f751fa8aa3e04cb76f931c6ec9f91f6e92
yongki <officeyongki@gmail.com>
2022-01-04 23:32:12 +0900

    algorithm update

--- src/algorithmProgrammers/java/level1/Solution12918.java
+++ src/algorithmProgrammers/java/level1/Solution12918.java
@@ -6,11 +6,43 @@ package algorithmProgrammers.java.level1;
 
 public class Solution12918 {
 	public boolean solution(String s) {
-        if(s.length() != 4 && s.length() != 6) return false;
-        for(int i=0; i<s.length(); i++) {
-        	char ch = s.charAt(i);
-        	if(ch < '0' || ch > '9') return false;
-        }
-        return true;
+		int len = s.length();
+		if (len == 4 || len == 6) {
+			for (char ch : s.toCharArray()) {
+				if (ch < '0' || ch > '9')
+					return false;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	public boolean solution2(String s) {
+		int len = s.length();
+		return (len == 4 || len == 6) && s.matches("\\d*");
+	}
+
+	public boolean solution3(String s) {
+		int len = s.length();
+		if (len == 4 || len == 6) {
+			try {
+				Integer.parseInt(s);
+				return true;
+			} catch (NumberFormatException e) {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		Solution12918 sol = new Solution12918();
+
+		sol.solution2("a234");
+		sol.solution2("1234");
+		sol.solution2("000a0");
+		sol.solution2("1");
+		sol.solution2("aaaa");
 	}
 }
