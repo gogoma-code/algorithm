d26618682146703623ef8bef76a4db8a62b94105
yongki <officeyongki@gmail.com>
2022-01-08 16:46:15 +0900

    update

--- src/algorithmProgrammers/java/level1/Solution12947.java
+++ src/algorithmProgrammers/java/level1/Solution12947.java
@@ -7,12 +7,32 @@ package algorithmProgrammers.java.level1;
 public class Solution12947 {
 	public boolean solution(int x) {
 		int sum = 0;
-		int tmpX = x;
-		while(tmpX != 0) {
-			sum += tmpX % 10;
-			tmpX /= 10;
+		for(int tmp=x; tmp>0; tmp/=10) {
+			sum += tmp % 10;
 		}
 		
-		return x % sum == 0 ? true : false;
+		return x % sum == 0;
+	}
+	
+	public boolean solution2(int x) {
+		int sum = 0;
+		for(char ch : String.valueOf(x).toCharArray()) {
+			sum += ch - 48;
+		}
+		
+		return x % sum == 0;
+	}
+	
+	public boolean solution3(int x) {
+		int sum = String.valueOf(x).chars().map(ch -> ch - 48).sum();
+		return x % sum == 0;
+	}
+	
+	public static void main(String[] args) {
+		Solution12947 sol = new Solution12947();
+		sol.solution2(10);
+		sol.solution2(11);
+		sol.solution2(12);
+		sol.solution2(13);
 	}
 }
