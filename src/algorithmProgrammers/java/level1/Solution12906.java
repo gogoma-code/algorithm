6c91971d2fe5c4b3378bf4b6d6123e03f8e7ccb2
lyk-brave <officeyongki@gmail.com>
2022-06-16 10:48:57 +0900

    programmers 12906 update

--- src/algorithmProgrammers/java/level1/Solution12906.java
+++ src/algorithmProgrammers/java/level1/Solution12906.java
@@ -8,14 +8,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution12906 {
-	public int[] solution(int []arr) {
+    public int[] solution(int[] arr) {
         List<Integer> list = new ArrayList<Integer>();
-        if(arr.length > 0) list.add(arr[0]);
-        for(int i=1; i<arr.length; i++) if(! list.get(list.size()-1).equals(arr[i])) list.add(arr[i]);
+        int saveNum = -1;
         
+        for (int a : arr) {
+            if (saveNum != a) list.add(saveNum = a);
+        }
+
         int[] newArr = new int[list.size()];
-        for(int i=0; i<newArr.length; i++) newArr[i] = list.get(i);
-        
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = list.get(i);
+        }
+
         return newArr;
-	}
+    }
+
+    public int[] solution2(int[] arr) {
+        List<Integer> list = new ArrayList<Integer>();
+        int saveNum = -1;
+
+        for (int a : arr) {
+            if (saveNum != a) list.add(saveNum = a);
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
