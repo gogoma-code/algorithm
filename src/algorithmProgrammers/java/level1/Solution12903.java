64904c984c2fc5ffe2a43a2a5e33cdc7032b5de3
lyk-brave <officeyongki@gmail.com>
2022-06-20 13:42:30 +0900

    12903

--- src/algorithmProgrammers/java/level1/Solution12903.java
+++ src/algorithmProgrammers/java/level1/Solution12903.java
@@ -6,10 +6,7 @@ package algorithmProgrammers.java.level1;
 
 public class Solution12903 {
 	public String solution(String s) {
-		String answer = "";
-		if(s.length() % 2 == 0) answer = s.substring(s.length()/2 - 1, s.length()/2+1);
-		else answer = Character.toString(s.charAt(s.length()/2));
-			
-		return answer;
+		int len = s.length();
+		return s.substring((len-1) / 2, (len/2) + 1);
 	}
 }
