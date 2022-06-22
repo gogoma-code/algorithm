f826ddca66dd6b0354782069128196007024775b
lyk-brave <officeyongki@gmail.com>
2022-06-22 09:36:16 +0900

    86491 new

---
+++ src/algorithmProgrammers/java/level1/Solution86491.java
@@ -0,0 +1,25 @@
+/***
+ * 최소직사각형
+ * https://programmers.co.kr/learn/courses/30/lessons/86491?language=java
+ */
+
+package algorithmProgrammers.java.level1;
+
+class Solution86491 {
+    public int solution(int[][] sizes) {
+        int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
+        for(int[] size : sizes) {
+            maxX = Math.max(maxX, Math.max(size[0], size[1]));
+            maxY = Math.max(maxY, Math.min(size[0], size[1]));
+        }
+        
+        return maxX * maxY;
+    }
+
+    public static void main(String[] args) {
+        Solution86491 sol = new Solution86491();
+        System.out.println(
+            sol.solution(new int[][] {{60, 50}, {30, 70}, {60, 30}, {80, 40}})
+        );
+    }
+}
