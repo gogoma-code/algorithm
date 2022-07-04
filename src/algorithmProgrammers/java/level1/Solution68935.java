a28e2c473cc1eb56f0c341999f26a49775c0263c
lyk-brave <officeyongki@gmail.com>
2022-07-04 11:20:32 +0900

    programmers 68935 update

--- src/algorithmProgrammers/java/level1/Solution68935.java
+++ src/algorithmProgrammers/java/level1/Solution68935.java
@@ -7,19 +7,11 @@ package algorithmProgrammers.java.level1;
 public class Solution68935 {
 	public int solution(int n) {		
 		StringBuilder sb = new StringBuilder();
-		int current = n;
-		int third = 3;
-		while(current > 0) {
-			sb.append( current % third < 10 ? current % third : (char)(current % third - 10 + 'A') );			
-			current /= third;
-		}		
+		while(n > 0) {
+			sb.append( n % 3 );			
+			n /= 3;
+		}
 		
 		return Integer.valueOf(sb.toString(), 3);
-	}
-	
-	public static void main(String[] args) {
-		Solution68935 sol = new Solution68935();
-		System.out.println(sol.solution(45));
-		System.out.println(sol.solution(125));
 	}
 }
