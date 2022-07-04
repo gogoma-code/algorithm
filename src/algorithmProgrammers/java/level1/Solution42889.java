fadd6c3682bc3c0b044a89fb749b6bbbd173340c
lyk-brave <officeyongki@gmail.com>
2022-07-04 17:54:25 +0900

    programmers 42889 update

--- src/algorithmProgrammers/java/level1/Solution42889.java
+++ src/algorithmProgrammers/java/level1/Solution42889.java
@@ -5,7 +5,9 @@
 package algorithmProgrammers.java.level1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Solution42889 {
 	public int[] solution(int N, int[] stages) {
@@ -13,9 +15,7 @@ public class Solution42889 {
 		List<Integer> answer_list = new ArrayList<>();
 		List<Double> failure_list = new ArrayList<>();
 		List<Integer> stages_list = new ArrayList<>(stages.length);
-		for(int i=0; i<stages.length; i++) {
-			stages_list.add(stages[i]);
-		}
+		for(int stage : stages) stages_list.add(stage);
 		
 		double failPercent = 0.0;
 		int failCnt = 0, totalCnt = 0;
@@ -28,6 +28,7 @@ public class Solution42889 {
 					j--;
 				}
 			}
+			
 			failPercent = (double)failCnt / totalCnt;
 			failCnt = 0;
 			failure_list.add(failPercent);
@@ -44,10 +45,46 @@ public class Solution42889 {
 			}
 		}
 		
-		
 		for(int i=0; i<answer.length; i++) {
 			answer[i] = answer_list.get(i);
 		}
 		return answer;
     }
+	
+	public int[] solution2(int N, int[] stages) {
+		List<Integer> stagesList = new ArrayList<Integer>();
+		List<Double> failureList = new ArrayList<>();
+		List<Integer> answerList = new ArrayList<>();
+		
+		for(int stage : stages) stagesList.add(stage);
+		
+		for(int i=1; i<=N; i++) {
+			int challenger = stagesList.size();
+			Iterator<Integer> iter = stagesList.iterator();
+			while(iter.hasNext()) {
+				if(iter.next() <= i) iter.remove();
+			}
+			
+			double failureRate = (double)(challenger - stagesList.size()) / challenger;
+			failureList.add(failureRate);
+			
+			for(int j=0; j<failureList.size(); j++) {
+				if(j == failureList.size()-1) {
+					answerList.add(i);
+					break;
+				}
+				if(failureRate > failureList.get(answerList.get(j)-1)) {
+					answerList.add(j, i);
+					break;
+				}
+			}
+		}
+		
+		int[] answer = new int[N];
+		for(int i=0; i<answer.length; i++) {
+			answer[i] = answerList.get(i);
+		}
+		
+		return answer;
+	}
 }
