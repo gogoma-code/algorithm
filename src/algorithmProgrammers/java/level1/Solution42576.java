06e037368f562b676daf996fa5ef7a49eabf41d7
lyk-brave <officeyongki@gmail.com>
2022-07-05 17:52:22 +0900

    programmers 42576 update

--- src/algorithmProgrammers/java/level1/Solution42576.java
+++ src/algorithmProgrammers/java/level1/Solution42576.java
@@ -8,18 +8,18 @@ import java.util.HashMap;
 
 public class Solution42576 {
 	public String solution(String[] participant, String[] completion) {
-        String notRunner = "";
+        String answer = new String();
         HashMap<String, Integer> map = new HashMap<String, Integer>();
         for(String runner : participant) map.put(runner, map.getOrDefault(runner, 0) + 1);
         for(String runner : completion) map.put(runner, map.get(runner) - 1);
 
         for(String runner : map.keySet()) {
             if(map.get(runner) != 0) {
-                notRunner = runner;
+            	answer = runner;
                 break;
             }
         }
 
-        return notRunner;
+        return answer;
     }
 }
