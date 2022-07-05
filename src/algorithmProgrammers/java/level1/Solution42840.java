7b5c0cbcc2a8a80eb46bc71614ffb519bcc2173d
lyk-brave <officeyongki@gmail.com>
2022-07-05 11:18:25 +0900

    programmers 42840 update

--- src/algorithmProgrammers/java/level1/Solution42840.java
+++ src/algorithmProgrammers/java/level1/Solution42840.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Solution42840 {
 	public int[] solution(int[] answers) {
@@ -38,5 +39,26 @@ public class Solution42840 {
 		for(int i=0; i<maxList.size(); i++) answer[i] = maxList.get(i);
 		
 		return answer;
+	}
+	
+	public int[] solution2(int[] answers) {
+		int supoja = 3;
+		int[][] supoja_pattern = {
+				{ 1, 2, 3, 4, 5 },
+				{ 2, 1, 2, 3, 2, 4, 2, 5 },
+				{ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 }
+		};
+		
+		int[] answerAry = new int[supoja];
+		for(int i=0; i<answerAry.length; i++) {
+			for(int j=0; j<answers.length; j++) {
+				if(supoja_pattern[i][j % supoja_pattern[i].length] == answers[j]) {					
+					answerAry[i]++;
+				}
+			}
+		}
+		
+		int max = Arrays.stream(answerAry).max().getAsInt();
+		return IntStream.range(1, supoja+1).filter(i -> answerAry[i-1] == max).toArray();
 	}
 }
