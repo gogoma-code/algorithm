cdfc92eab0fc1a0c7d69980849db7096d2acffad
lyk-brave <officeyongki@gmail.com>
2022-07-07 09:52:57 +0900

    progreammers 64061 update

--- src/algorithmProgrammers/java/level1/Solution64061.java
+++ src/algorithmProgrammers/java/level1/Solution64061.java
@@ -8,21 +8,20 @@ import java.util.Stack;
 
 public class Solution64061 {
 	public int solution(int[][] board, int[] moves) {
-		int answer = 0;
+		int answer = 0, doll = 0;
 	    Stack<Integer> stack = new Stack<Integer>();
-	    int pushItem = 0;
 	    
 	    for(int move : moves) {
 	    	move--; 
 	    	for(int i=0; i<board.length; i++) { 
-	    		pushItem = board[i][move];
-	    		if(pushItem > 0) {
+	    		doll = board[i][move];
+	    		if(doll > 0) {
 	    			board[i][move] = 0; 
-	    			if(!stack.isEmpty() && pushItem == stack.peek()) { 
+	    			if(!stack.isEmpty() && doll == stack.peek()) { 
 	    				stack.pop();
 	    				answer += 2;
 	    			} else { 
-	    				stack.push(pushItem);
+	    				stack.push(doll);
 	    			}
 	    			break;
 	    		}
@@ -30,10 +29,5 @@ public class Solution64061 {
 	    }
 	    
 	    return answer;
-	}
-	
-	public static void main(String[] args) {
-		Solution64061 sol = new Solution64061();
-		System.out.println(sol.solution(new int[][] {{0,0,0,0,0}, {0,0,1,0,3}, {0,2,5,0,1}, {4,2,4,4,2}, {3,5,1,3,1}}, new int[] {1, 5, 3, 5, 1, 2, 1, 4}));
 	}
 }
