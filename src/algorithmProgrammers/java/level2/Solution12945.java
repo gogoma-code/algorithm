7cdd792c25d200b776bda46c2660201a3c35e821
lyk-brave <officeyongki@gmail.com>
2022-07-15 10:22:11 +0900

    prm 12945 피보나치 수 update

--- src/algorithmProgrammers/java/level2/Solution12945.java
+++ src/algorithmProgrammers/java/level2/Solution12945.java
@@ -5,10 +5,27 @@
 package algorithmProgrammers.java.level2;
 
 public class Solution12945 {
+	// 재귀함수 - 시간초과
+	/*
 	public int solution(int n) {
+		int mod = 1234567;
 		if(n == 1 || n == 2) return 1;
-		int f = solution(n-2) + solution(n-1);
-		f %= 1234567;
+		int f = ( (solution(n-2)%mod) + (solution(n-1)%mod) ) % mod;
 		return f;
+	}
+	*/
+	
+	public int solution2(int n) {
+		int count=2, mod=1234567;
+		int f1=0, f2=1;
+		
+		while(n > count) {
+			int temp = f2;
+			f2 = ( (f1%mod) + (f2%mod) ) % mod;
+			f1 = temp;
+			count++;
+		}
+		
+		return (f1+f2) % mod;
 	}
 }
