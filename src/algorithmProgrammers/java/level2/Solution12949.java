65d4cfe0e9fcb17bdcecfde199217a3c16f1d1ee
lyk-brave <officeyongki@gmail.com>
2022-07-15 16:03:22 +0900

    prm 행렬의 곱셈 update

--- src/algorithmProgrammers/java/level2/Solution12949.java
+++ src/algorithmProgrammers/java/level2/Solution12949.java
@@ -6,15 +6,15 @@ package algorithmProgrammers.java.level2;
 
 public class Solution12949 {
 	public int[][] solution(int[][] arr1, int[][] arr2) {
-		int[][] newArr = new int[arr1.length][arr2[0].length];
-		for(int i=0; i<newArr.length; i++) {
-			for(int j=0; j<newArr[0].length; j++) {
+		int[][] answer = new int[arr1.length][arr2[0].length];
+		for(int i=0; i<answer.length; i++) {
+			for(int j=0; j<answer[0].length; j++) {
 				for(int k=0; k<arr1[0].length; k++) {
-					newArr[i][j] += arr1[i][k] * arr2[k][j];
+					answer[i][j] += arr1[i][k] * arr2[k][j];
 				}
 			}
 		}
 		
-		return newArr;
+		return answer;
 	}
 }
