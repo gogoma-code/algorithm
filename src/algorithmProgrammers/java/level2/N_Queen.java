9a77c35f196793144bb257763c8ab746b0ca26a8
lyk-brave <officeyongki@gmail.com>
2022-07-19 17:37:19 +0900

    prm level2 N-Queen

---
+++ src/algorithmProgrammers/java/level2/N_Queen.java
@@ -0,0 +1,49 @@
+/**
+ * N-Queen
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12952?language=java
+ */
+package algorithmProgrammers.java.level2;
+
+import java.util.Arrays;
+
+public class N_Queen {
+	public int solution(int n) {		
+		int answer = 0;
+		
+		for(int first=0; first<n; first++) {
+			int[] board = new int[n]; // 행은 인덱스, 열은 값
+			Arrays.fill(board, -1);
+			
+			board[0] = first; 
+			answer += checkPutTheQueenAll(board, n, 1);
+		}
+		
+        return answer;
+    }
+	
+	public int checkPutTheQueenAll(int[] board, int n, int row) {
+		if(row == n) return 1;
+		
+		int count = 0;
+		for(int col=0; col<n; col++) {
+			boolean checkPut = true;
+			for(int pick=0; pick<row; pick++) {
+				if(col == board[pick] || row-pick == Math.abs(col-board[pick])) {
+					checkPut = false;
+					break;
+				}
+			}
+			
+			if(checkPut) {
+				board[row] = col;
+				count += checkPutTheQueenAll(board, n, row+1);
+			} 
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		N_Queen sol = new N_Queen();
+		sol.solution(4);
+	}
+}
