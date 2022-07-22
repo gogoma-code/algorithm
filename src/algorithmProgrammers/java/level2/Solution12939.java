486d16aa62f607f2a483dfc5619977e7fedf6d37
lyk-brave <officeyongki@gmail.com>
2022-07-22 14:15:04 +0900

    prm 12939 update

--- src/algorithmProgrammers/java/level2/Solution12939.java
+++ src/algorithmProgrammers/java/level2/Solution12939.java
@@ -8,9 +8,26 @@ import java.util.Arrays;
 
 public class Solution12939 {
 	public String solution(String s) {
-		String[] sArr = s.split(" ");		
-		Arrays.sort(sArr);
-		
-		return sArr[0] + " " + sArr[sArr.length-1];
+		String[] sArr = s.split(" ");
+
+		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+		for (String str : sArr) {
+			int num = Integer.parseInt(str);
+			
+			if (min > num) { min = num; }
+			if (max < num) { max = num; }
+		}
+
+		return min + " " + max;
+	}
+
+	public String solution2(String s) {
+		String[] sArr = s.split(" ");
+		int[] iArr = new int[sArr.length];
+		for (int i = 0; i < sArr.length; i++) { iArr[i] = Integer.parseInt(sArr[i]); }
+
+		Arrays.sort(iArr);
+
+		return iArr[0] + " " + iArr[iArr.length - 1];
 	}
 }
