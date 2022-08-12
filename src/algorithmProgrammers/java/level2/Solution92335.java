129857db4c9fcc9ced373541ec67289774d090c0
lyk-brave <officeyongki@gmail.com>
2022-08-12 13:50:18 +0900

    k진수에서 소수 개수 구하기

--- src/algorithmProgrammers/java/level2/Solution92335.java
+++ src/algorithmProgrammers/java/level2/Solution92335.java
@@ -1,12 +1,72 @@
+/**
+ * k진수에서 소수 개수 구하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=java
+ */
 package algorithmProgrammers.java.level2;
 
 public class Solution92335 {
 	public int solution(int n, int k) {
-        int answer = -1;
-        return answer;
-    }
+		StringBuilder sb = new StringBuilder();
+		while (n > 0) {
+			sb.insert(0, n % k);
+			n /= k;
+		}
+		String n_to_k = sb.toString();
+
+		int count = 0;
+		String[] splitZero = n_to_k.split("0");
+		for(String str : splitZero) {
+			if(!str.trim().isEmpty()) {
+				long num = Long.parseLong(str);
+				if (isPrime(num)) count++;
+			}
+		}
+
+		return count;
+	}
 	
-	public static void main(String[] args) {
+	public int solution2(int n, int k) {
+		int count = 0;
+		StringBuilder sb = new StringBuilder();
+		while (n > 0) {
+			int ins = n % k;
+			if(ins != 0) {
+				sb.insert(0, n % k);
+			} else {
+				if(sb.length() > 0) {
+					long num = Long.parseLong(sb.toString());
+					if (isPrime(num)) count++;
+				}
+				sb.setLength(0);
+			}
+			n /= k;
+		}
+		if(sb.length() > 0) {
+			long num = Long.parseLong(sb.toString());
+			if (isPrime(num)) count++;
+		}
 		
+		return count;
+	}
+
+	public boolean isPrime(long num) {
+		boolean isPrime = true;
+		for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		return num <= 1 ? false : isPrime;
+	}
+
+	public static void main(String[] args) {
+		Solution92335 sol = new Solution92335();
+//		int[] i = { 437674, 3 };
+		int[] i = { 110011, 10 };
+//		int[] i = { 2, 3 };
+		
+		int out = sol.solution2(i[0], i[1]);
+		System.out.println(out);
 	}
 }
