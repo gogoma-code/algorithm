7c1bcdf5391c8861209f15bcf2c1adf8312be748
lyk-brave <officeyongki@gmail.com>
2022-08-31 14:33:41 +0900

    n^2 배열 자르기

---
+++ src/algorithmProgrammers/java/level2/Solution87390.java
@@ -0,0 +1,33 @@
+/**
+ * n^2 배열 자르기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/87390?language=java
+ */
+package algorithmProgrammers.java.level2;
+
+import java.util.Arrays;
+
+public class Solution87390 {
+	public int[] solution(int n, long left, long right) {		
+		int startIndex = (int) (left % n);
+		int len = (int) (right - left) + 1;
+		int[] arr = new int[len];
+		int standardValue = (int) (left / n) + 1;
+		
+		int i = 0;
+		int idx = startIndex, val = startIndex+1;
+		while(i < len) {
+			arr[i++] = Math.max(val, standardValue);
+			
+			idx++;
+			if(idx % n == 0) {
+				val = 1;
+				standardValue++;
+			} else {
+				val++;
+			}
+		}
+				
+		
+        return arr;
+    }
+}
