0338fdd91914abde998a1582363e05fe4e3bfa7d
lyk-brave <officeyongki@gmail.com>
2022-08-31 12:58:16 +0900

    init

--- src/algorithmProgrammers/java/level1/Solution118666.java
+++ src/algorithmProgrammers/java/level1/Solution118666.java
@@ -18,33 +18,22 @@ class Solution118666 {
 			if (!sv.isEmpty()) map.put(sv, map.getOrDefault(sv, 0) + choice);
 		}
 		
-		return getMBTIFull(map);
+		
+		return getMBTI(map);
 	}
 	
-	public String getMBTIFull(Map<String, Integer> map) {
+	public String getMBTI(Map<String, Integer> map) {
 		String[] mbtiGroup = { "RT", "CF", "JM", "AN" };
 		String mbti = new String();
 		for(String mg : mbtiGroup) {
-			mbti += getMBTIPick(map, String.valueOf(mg.charAt(0)), String.valueOf(mg.charAt(1)));
+			String first = String.valueOf(mg.charAt(0));
+			String second = String.valueOf(mg.charAt(1));
+			int score = map.getOrDefault(first, 0) - map.getOrDefault(second, 0);
+			String pick = score < 0 ? second : first;
+			
+			mbti += pick;
 		}
 		
 		return mbti;
-	}
-	
-	public String getMBTIPick(Map<String, Integer> map, String first, String second) {
-		int one = map.getOrDefault(first, 0) - map.getOrDefault(second, 0);
-		return one < 0 ? second : first;
-	}
-
-	public static void main(String[] args) {
-		Solution118666 sol = new Solution118666();
-//		String[] survey = { "AN", "CF", "MJ", "RT", "NA" };
-//		int[] choices = { 5, 3, 2, 7, 5 };
-
-		String[] survey = { "TR", "RT", "TR" };
-		int[] choices = { 7, 1, 3 };
-
-		String mbti = sol.solution(survey, choices);
-		System.out.println(mbti);
 	}
 }
